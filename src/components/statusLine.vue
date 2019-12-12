af988fe7cb9afdcd333935e082e69fe82d3919f5
SindreMA <sindrema@gmail.com>
2019-12-12 07:05:24 +0100

    Update statusLine.vue

--- src/components/statusLine.vue
+++ src/components/statusLine.vue
@@ -1,33 +1,36 @@
 <template>
-    <div class="flex">
-        <div class="circle" :class="{'bg-ligthGreen': status.toLowerCase() === 'sent', 'bg-ligthRed': status.toLowerCase() == 'not sent', 'bg-ligthYellow': status.toLowerCase()  == 'in progress'}"></div>
-        <div class="centerText"> {{status}}</div>
-    </div>
+  <div class="flex">
+    <div
+      class="circle"
+      :class="{'bg-ligthGreen': status.toLowerCase() === 'sent', 'bg-ligthRed': status.toLowerCase() == 'not sent', 'bg-ligthYellow': status.toLowerCase()  == 'in progress'}"
+    ></div>
+    <div class="centerText">{{status}}</div>
+  </div>
 </template>
 
 <script>
 export default {
-    props: ['status']
-}
+  props: ["status"]
+};
 </script>
 
 <style scoped>
 .circle {
-    height: 16px;
-    width: 16px;
-    border-radius: 100%;
-    margin-right: 5px;
+  height: 16px;
+  width: 16px;
+  border-radius: 100%;
+  margin-right: 5px;
 }
 .bg-ligthGreen {
-    background-color: #B8DE91;
+  background-color: #b8de91;
 }
 .bg-ligthYellow {
-    background-color: #FBD277;
+  background-color: #fbd277;
 }
 .bg-ligthRed {
-    background-color: #FB9683;
+  background-color: #fb9683;
 }
 .centerText {
-    text-align: center;
+  text-align: center;
 }
 </style>
