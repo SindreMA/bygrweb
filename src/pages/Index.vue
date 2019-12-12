3fde4481b149c277c11240e8fd199c41fbacba2c
SindreMA <sindrema@gmail.com>
2019-12-12 07:05:04 +0100

    Update Index.vue

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,105 +1,103 @@
 <template>
   <q-page class="minorMargin minorPadding">
-      <q-select
+    <q-select
       dense
-        v-model="filterType"
-        use-input
-        input-debounce="0"
-        :options="filterOptions"
-        @filter="filterFn"
-        label="Filter on type"
-        style="width: 280px"
-        behavior="menu"
-        outlined
-        clearable
-      >
-        <template v-slot:no-option>
-          <q-item>
-            <q-item-section class="text-grey">
-              No results
-            </q-item-section>
-          </q-item>
-        </template>
-      </q-select>
-
-
+      v-model="filterType"
+      use-input
+      input-debounce="0"
+      :options="filterOptions"
+      @filter="filterFn"
+      label="Filter on type"
+      style="width: 280px"
+      behavior="menu"
+      outlined
+      clearable
+    >
+      <template v-slot:no-option>
+        <q-item>
+          <q-item-section class="text-grey">No results</q-item-section>
+        </q-item>
+      </template>
+    </q-select>
 
     <div v-if="items" class="minorPadding">
-      <div v-for="item in items.filter(x=> {return filterType == null || x.layoutType == filterType})" :key="item.id">
-          <buildingInfoBox :data="item" />
+      <div
+        v-for="item in items.filter(x=> {return filterType == null || x.layoutType == filterType})"
+        :key="item.id"
+      >
+        <buildingInfoBox :data="item" />
       </div>
     </div>
-  </q-page> 
+  </q-page>
 </template>
 
 <script>
-import buildingInfoBox from '../components/buildingInfoBox'
-import axios from 'axios'
+import buildingInfoBox from "../components/buildingInfoBox";
+import axios from "axios";
 export default {
-  name: 'PageIndex',
+  name: "PageIndex",
   components: {
     buildingInfoBox
   },
   data() {
-      return {
-        items: null,
-        filterType: null,
-        filterOptions: this.getTypes,
-        
-    }
+    return {
+      items: null,
+      filterType: null,
+      filterOptions: this.getTypes
+    };
   },
   computed: {
-        getTypes() {
-      var ls = []
-          if (this.items) {
-            this.items.forEach(item => {
-            if (ls.filter(x=> x ==  item.layoutType).length == 0) {
-              ls.push(item.layoutType)
-            }
-          });  
+    getTypes() {
+      var ls = [];
+      if (this.items) {
+        this.items.forEach(item => {
+          if (ls.filter(x => x == item.layoutType).length == 0) {
+            ls.push(item.layoutType);
           }
-          
-          return ls;
-    },
+        });
+      }
+
+      return ls;
+    }
   },
   methods: {
-    
-    filterFn (val, update, abort) {
-      if (val === '') {
+    filterFn(val, update, abort) {
+      if (val === "") {
         update(() => {
-          this.filterOptions = this.getTypes
-        })
-        return
+          this.filterOptions = this.getTypes;
+        });
+        return;
       }
 
       update(() => {
-        const needle = val.toLowerCase()
-        this.filterOptions = this.getTypes.filter(v => v.toLowerCase().indexOf(needle) > -1)
-      })
-    },
+        const needle = val.toLowerCase();
+        this.filterOptions = this.getTypes.filter(
+          v => v.toLowerCase().indexOf(needle) > -1
+        );
+      });
+    }
   },
-  created () {
-    var vm = this
-    axios.get("https://bygrapi.sindrema.com/api/buyer-info").then(x=> {
-      if (x.data) {
-        vm.items = x.data
-      }
-    }).catch(x=> {
-      console.log(x);
-    })
+  created() {
+    var vm = this;
+    axios
+      .get("https://bygrapi.sindrema.com/api/buyer-info")
+      .then(x => {
+        if (x.data) {
+          vm.items = x.data;
+        }
+      })
+      .catch(x => {
+        console.log(x);
+      });
   }
-}
+};
 </script>
 
 <style scoped>
 .minorMargin {
   margin: 20px;
 }
-.hoverGlow:hover {
-  border: 1px blue solid;
-      text-decoration: blue;
 
-}
 .minorBorder {
   border: rgb(160, 160, 160) 1px solid;
   border-radius: 10px;
@@ -107,5 +105,4 @@ export default {
 .minorPadding {
   padding: 20px;
 }
-
 </style>
